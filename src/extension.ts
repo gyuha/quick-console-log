667cf76a8155563f5bcdf0d811530e307643f290
gyuha <nicegyuha@gmail.com>
2021-02-05 17:44:00 +0900

    윗줄 로그 추가

--- src/extension.ts
+++ src/extension.ts
@@ -66,54 +66,29 @@ function handle(direction: Wrap, prefix?: boolean, type?: string) {
       item,
       doc,
       language,
-      direction === Wrap.down ? lineNumber + 1 : lineNumber,
+      direction === Wrap.up ? lineNumber + 2 : lineNumber + 1,
       properties
     );
-
-    const getTargetLine = () => {
-      let stop = false;
-      let li = lineNumber;
-      let l = 0;
-      while (!stop) {
-        if (direction === Wrap.down) {
-        } else {
-          li--;
-        }
-        if (li < doc.lineCount) {
-          if (!doc.lineAt(li).isEmptyOrWhitespace) {
-            l = li;
-            stop = true;
-          }
-        } else {
-          if (li === doc.lineCount) {
-            li--;
-            stop = true;
-          }
-        }
-      }
-      return li;
-    };
 
     // insert text
     let printLineNumber = direction === Wrap.down ? lineNumber : lineNumber - 1;
     printLineNumber = printLineNumber < 0 ? 0 : printLineNumber;
 
-    let nxtLine: vscode.TextLine;
-    let nxtLineInd: string;
-
-    if (!(doc.lineCount - 1 === lineNumber)) {
-      nxtLine = doc.lineAt(lineNumber + 1);
-      nxtLineInd = nxtLine.text.substring(
-        0,
-        nxtLine.firstNonWhitespaceCharacterIndex
-      );
-    } else {
-      nxtLineInd = "";
-    }
-
     switch (direction) {
       // down
       case Wrap.down:
+        let nxtLine: vscode.TextLine;
+        let nxtLineInd: string;
+
+        if (!(doc.lineCount - 1 === lineNumber)) {
+          nxtLine = doc.lineAt(lineNumber + 1);
+          nxtLineInd = nxtLine.text.substring(
+            0,
+            nxtLine.firstNonWhitespaceCharacterIndex
+          );
+        } else {
+          nxtLineInd = "";
+        }
         currentEditor
           .edit((e) => {
             e.insert(
@@ -131,18 +106,20 @@ function handle(direction: Wrap, prefix?: boolean, type?: string) {
 
       // up
       case Wrap.up:
+        const tLine = doc.lineAt(lineNumber === 0 ? 0 : lineNumber - 1);
+        const tLineEmpty = tLine.text.trim() === "";
+        let lineCorr = 0;
+
         currentEditor
           .edit((e) => {
-            e.insert(
-              new vscode.Position(
-                lineNumber,
-                doc.lineAt(printLineNumber).range.end.character
-              ),
-              "\n".concat(nxtLineInd > ind ? nxtLineInd : ind, txt)
-            );
+              e.insert(
+                new vscode.Position(lineNumber, 0),
+                ind.concat(txt, "\n")
+              );
           })
           .then(() => {
-            currentEditor.selection = sel;
+            let newSel = new vscode.Position(lineNumber + 1, sel.anchor.character);
+            currentEditor.selection = new vscode.Selection(newSel, newSel);
           });
         break;
 
